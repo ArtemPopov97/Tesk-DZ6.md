83b7c059a76e5ae94a549f2752164c091562aff6
unknown <artem.popov.97@mail.ru>
2022-06-30 12:22:18 +0300

    Tesk-Zero-Array

---
+++ Tesk-1-DZ/Program.cs
@@ -0,0 +1,50 @@
+﻿// Пользователь вводит с клавиатуры М чисел. Посчитать,
+// сколько чисел больше 0 ввел пользователь 
+
+
+int [] FillArray() 
+{
+    Console.WriteLine("Введите размер массива");
+    int A = Convert.ToInt32(Console.ReadLine());
+    int [] array = new int [A];
+    for (int i = 0; i < A; i++)
+    {
+        Console.WriteLine("Введите число");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+
+void PrintArray(int[] array) 
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ,");
+    }
+    Console.WriteLine();
+}
+
+void Zero (int [] array)   
+{
+    int N = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] > 0)
+        {
+            N = N + 1;
+        }
+       
+        else 
+        {
+            N = N + 0;
+        }
+    } Console.WriteLine($"Колличество положительных чисел = {N} ");
+
+}
+
+int [] Mas = FillArray();
+
+PrintArray(Mas);
+
+Zero(Mas);
